1556160c57da5a2fd4646157b5f258dcf6b6012a
Mathieu Bourdier <math.9408@gmail.com>
2024-10-16 17:30:26 +0200

    stevenson movies & actor

---
+++ chlg4.php
@@ -0,0 +1,20 @@
+<?php
+$movies = [
+    ["Docteur Jekyll","Eddie Izzard", "Scott Chambers", "Robyn Cara"],
+    ["Le Club du suicide","Oleg Dahl", "Donatas Banionis", "Igor Dmitriev"],
+    ["Kidnapped","Mickael Cain", "Lawrence Douglas", "Trevor Howard"]
+    ];
+    /*Movies & Actor*/
+
+   foreach ($movies[0] as $movie) {
+    echo "$movie .";
+     }
+     echo "\n";
+    foreach ($movies[1] as $movie) {
+    echo "$movie .";
+     }
+     echo "\n";
+    foreach ($movies[2] as $movie) {
+     echo "$movie .";
+     }
+    
